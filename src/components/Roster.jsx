589bad3ee6541db3c91c75f404b58084813c4327
iandab47 <dabinett.ian@gmail.com>
2022-10-05 18:32:37 -0500

    added my component

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,11 +1,13 @@
 // imports
 import Gabe from './students/Gabe'
+import Ian from './students/Ian'
 
 export default function Roster() {
     return (
       <>
         <h2>I am a Roster!</h2>
         <Gabe />
+        <Ian />
       </>
     )
   }
---
+++ src/components/students/Ian.jsx
@@ -0,0 +1,7 @@
+export default function Ian() {
+  return (
+    <>
+      <h2>Ian is Ian</h2>
+    </>
+  )
+}
